3120f972c6a46ede3c18b901904fc429eb6dec1f
officialNamansharma <hey.namansharma@gmail.com>
2026-01-15 23:20:24 +0530

    Solved Variable Declaration and Updation

---
+++ Introduction/Variable_Declaration_and_Updation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 3;
+        // Write your code here
+        System.out.println(x);
+        System.out.println(x);
+    }
+}
