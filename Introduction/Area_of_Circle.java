b18aa87a76ec00229b91df61e8ad53168a81ec3b
officialNamansharma <hey.namansharma@gmail.com>
2026-01-15 23:56:05 +0530

    Solved Area of Circle

---
+++ Introduction/Area_of_Circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Write your code here
+        double r = 5;
+        double a = 3.14*r*r;
+        System.out.println(a);
+    }
+}
