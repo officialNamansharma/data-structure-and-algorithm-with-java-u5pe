1460709882ee8e14069098d706cb9e13777b0c71
officialNamansharma <hey.namansharma@gmail.com>
2026-01-16 00:12:41 +0530

    Solved Simple Interest

---
+++ Introduction/Simple_Interest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int p = 5000;
+        int r = 5;
+        int t = 2;
+        int si = p*r*t/100;
+        System.out.println(si);
+    }
+}
