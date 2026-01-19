80a04999a551ede2b5bad3cbdafc866c7a76cbb5
officialNamansharma <hey.namansharma@gmail.com>
2026-01-19 17:18:57 +0530

    Solved Check if the number is 3 digit number or not

---
+++ Some_Practice_Questions/Check_if_the_number_is_3_digit_number_or_not.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class classname{
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    
+        if (n<1000 && n>99) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+       
+    }
+}
