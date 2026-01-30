ca27df737fa8440eb694b11b8ad664a95595cac1
Naman Sharma <hey.namansharma@gmail.com>
2026-01-31 02:38:41 +0530

    Solved Sum

---
+++ Loops_-_3/Sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class main 
+{
+    public static void  main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum=0;
+        while(n != 0)
+        {
+            int ld = n%10;
+            sum += ld;
+            n /= 10;
+        }
+        System.out.println(sum);
+
+    }
+}
